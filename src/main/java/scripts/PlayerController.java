3d987215cac61478323b28a81d97c892b0ca12f5
lucasstarsz <andrewrcdey@gmail.com>
2021-12-22 22:33:09 -0500

    add mouse rotation movement

--- src/main/java/scripts/PlayerController.java
+++ src/main/java/scripts/PlayerController.java
@@ -1,5 +1,6 @@
 package scripts;
 
+import tech.fastj.engine.FastJEngine;
 import tech.fastj.math.Pointf;
 import tech.fastj.graphics.game.GameObject;
 
@@ -7,6 +8,8 @@ import tech.fastj.input.InputManager;
 import tech.fastj.input.keyboard.Keyboard;
 import tech.fastj.input.keyboard.KeyboardActionListener;
 import tech.fastj.input.keyboard.Keys;
+import tech.fastj.input.mouse.MouseActionListener;
+import tech.fastj.input.mouse.events.MouseMotionEvent;
 import tech.fastj.systems.behaviors.Behavior;
 
 public class PlayerController implements Behavior {
@@ -15,6 +18,7 @@ public class PlayerController implements Behavior {
     private final Runnable playerObserver;
 
     private KeyboardActionListener keyListener;
+    private MouseActionListener mouseListener;
     private Pointf movement;
     private float movementSpeed;
     private float rotation;
@@ -62,6 +66,44 @@ public class PlayerController implements Behavior {
         };
 
         inputManager.addKeyboardActionListener(keyListener);
+
+        mouseListener = new MouseActionListener() {
+            @Override
+            public void onMouseMoved(MouseMotionEvent mouseMotionEvent) {
+                Pointf mousePosition = mouseMotionEvent.getMouseLocation();
+                Pointf playerCenter = player.getCenter();
+                Pointf currentDirection = Pointf.up()
+                        .rotate(-player.getRotationWithin360())
+                        .add(playerCenter);
+
+                double distanceA = Pointf.distance(currentDirection, mousePosition);
+                double distanceB = Pointf.distance(mousePosition, playerCenter);
+                double distanceC = Pointf.distance(playerCenter, currentDirection);
+                double distanceASquared = distanceA * distanceA;
+                double distanceBSquared = distanceB * distanceB;
+                double distanceCSquared = distanceC * distanceC;
+                float angle = (float) Math.toDegrees(Math.acos(
+                        (distanceBSquared + distanceCSquared - distanceASquared) / (2 * distanceB * distanceC)
+                ));
+
+                if (!Float.isNaN(angle)) {
+                    Pointf newDirection = Pointf.up().rotate(-player.getRotationWithin360() + angle).add(playerCenter);
+                    Pointf oppositeNewDirection = Pointf.up().rotate(-player.getRotationWithin360() - angle).add(playerCenter);
+                    double newDistance = Pointf.distance(newDirection, mousePosition);
+                    double oppositeNewDistance = Pointf.distance(oppositeNewDirection, mousePosition);
+
+                    if (newDistance > oppositeNewDistance) {
+                        FastJEngine.log("Rotate {} degrees right", angle);
+                        player.setRotation((player.getRotationWithin360() + angle) % 360);
+                    } else {
+                        FastJEngine.log("Rotate {} degrees left", angle);
+                        player.setRotation((player.getRotationWithin360() - angle) % 360);
+                    }
+                }
+            }
+        };
+
+        inputManager.addMouseActionListener(mouseListener);
     }
 
     @Override
@@ -72,6 +114,7 @@ public class PlayerController implements Behavior {
     public void destroy() {
         inputManager.removeKeyboardActionListener(keyListener);
         keyListener = null;
+        mouseListener = null;
         movement = null;
         movementSpeed = 0f;
         rotation = 0f;
